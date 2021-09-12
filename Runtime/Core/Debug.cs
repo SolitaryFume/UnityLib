355c63b366f4615e86905b9b858a265b8ca705aa
骑龙巷左护法 <996483674@qq.com>
2021-09-12 16:56:31 +0800

    编译条件

--- Runtime/Core/Debug.cs
+++ Runtime/Core/Debug.cs
@@ -9,92 +9,120 @@ namespace UnityLib
 {
     public static class Debug
     {
-        [Conditional("Log")] [DebuggerHidden]
+        [Conditional("Log")]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void Log(object message)
         {
             UnityEngine.Debug.Log(message);
         }
 
-        [Conditional("Log")] [DebuggerHidden]
+        [Conditional("Log")]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void Log(object message, Object context)
         {
             UnityEngine.Debug.Log(message,context);
         }
 
-        [Conditional("Log")] [DebuggerHidden]
+        [Conditional("Log")]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogFormat(string format, params object[] args)
         {
             UnityEngine.Debug.LogFormat(format,args:args);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")]
+        [Conditional("Log")]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
         [DebuggerHidden]
         public static void LogFormat(Object context, string format, params object[] args)
         {
             UnityEngine.Debug.LogFormat(context, format, args: args);
         }
 
-        [Conditional("Log")][DebuggerHidden]
+        [Conditional("Log")]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, params object[] args)
         {
             UnityEngine.Debug.LogFormat(logType, logOptions, context, format,args: args);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogError(object message)
         {
             UnityEngine.Debug.LogError(message);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogError(object message, Object context)
         {
             UnityEngine.Debug.LogError(message, context);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogErrorFormat(string format, params object[] args)
         {
             UnityEngine.Debug.LogErrorFormat(format, args: args);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogErrorFormat(Object context, string format, params object[] args)
         {
             UnityEngine.Debug.LogErrorFormat(context,format, args: args);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogException(Exception exception)
         {
             UnityEngine.Debug.LogException(exception);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][Conditional("LogError")][DebuggerHidden]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogException(Exception exception, Object context)
         {
             UnityEngine.Debug.LogException(exception,context);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][DebuggerHidden]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogWarning(object message)
         {
             UnityEngine.Debug.LogWarning(message);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][DebuggerHidden]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogWarning(object message, Object context)
         {
             UnityEngine.Debug.LogWarning(message, context);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][DebuggerHidden]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogWarningFormat(string format, params object[] args)
         {
             UnityEngine.Debug.LogWarningFormat(format, args: args);
         }
 
-        [Conditional("LogWarning")][Conditional("Log")][DebuggerHidden]
+        [Conditional("LogWarning")]
+        [Conditional("LogError")]
+        [DebuggerHidden]
         public static void LogWarningFormat(Object context, string format, params object[] args)
         {
             UnityEngine.Debug.LogWarningFormat(context,format, args: args);
