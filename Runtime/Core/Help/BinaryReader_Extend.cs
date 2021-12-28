69a1cbb117e47ee52509fe19fc164f7b1b329db3
詹家昶 <zhanjiachang@021.com>
2021-12-28 13:14:34 +0800

    二进制流读写扩展

---
+++ Runtime/Core/Help/BinaryReader_Extend.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace UnityLib
+{
+    public static class BinaryReader_Extend
+    {
+        public static Vector2 ReadVector2(this BinaryReader reader)
+        {
+            return new Vector2()
+            {
+                x = reader.ReadSingle(),
+                y = reader.ReadSingle(),
+            };
+        }
+
+        public static Vector3 ReadVector3(this BinaryReader reader)
+        {
+            return new Vector3()
+            {
+                x = reader.ReadSingle(),
+                y = reader.ReadSingle(),
+                z = reader.ReadSingle(),
+            };
+        }
+
+        public static Vector4 ReadVector4(this BinaryReader reader)
+        {
+            return new Vector4()
+            {
+                x = reader.ReadSingle(),
+                y = reader.ReadSingle(),
+                w = reader.ReadSingle(),
+                z = reader.ReadSingle(),
+            };
+        }
+
+        public static Vector2Int ReadVector2Int(this BinaryReader reader)
+        {
+            return new Vector2Int()
+            {
+                x = reader.ReadInt32(),
+                y = reader.ReadInt32(),
+            };
+        }
+
+        public static Vector3Int ReadVector3Int(this BinaryReader reader)
+        {
+            return new Vector3Int()
+            {
+                x = reader.ReadInt32(),
+                y = reader.ReadInt32(),
+                z = reader.ReadInt32(),
+            };
+        }
+
+        public static Quaternion ReadQuaternion(this BinaryReader reader)
+        {
+            return new Quaternion()
+            {
+                x = reader.ReadSingle(),
+                y = reader.ReadSingle(),
+                z = reader.ReadSingle(),
+                w = reader.ReadSingle(),
+            };
+        }
+
+        public static Color ReadColor(this BinaryReader reader)
+        {
+            return new Color()
+            {
+                r = reader.ReadSingle(),
+                g = reader.ReadSingle(),
+                b = reader.ReadSingle(),
+                a = reader.ReadSingle(),
+            };
+        }
+
+        public static void Read(this BinaryReader reader, ref Vector2 value)
+        {
+            value.Set(reader.ReadSingle(), reader.ReadSingle());
+        }
+
+        public static void Read(this BinaryReader reader, ref Transform transform)
+        {
+            if (transform == null)
+                throw new ArgumentNullException(nameof(transform));
+            transform.SetPositionAndRotation(reader.ReadVector3(), reader.ReadQuaternion());
+            transform.localScale = reader.ReadVector3();
+        }
+
+        public static Vector3[] ReadVector3Array(this BinaryReader reader)
+        {
+            var isnull = reader.ReadBoolean();
+            if(isnull)
+                return null;
+            var l = reader.ReadInt32();
+            var array = new Vector3[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = reader.ReadVector3();
+            }
+            return array;
+        }
+
+        public static Vector2[] ReadVector2Array(this BinaryReader reader)
+        {
+            var isnull = reader.ReadBoolean();
+            if (isnull)
+                return null;
+            var l = reader.ReadInt32();
+            var array = new Vector2[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = reader.ReadVector2();
+            }
+            return array;
+        }
+
+        public static int[] ReadIntArray(this BinaryReader reader)
+        {
+            var isnull = reader.ReadBoolean();
+            if (isnull)
+                return null;
+            var l = reader.ReadInt32();
+            var array = new int[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = reader.ReadInt32();
+            }
+            return array;
+        }
+        public static Color[] ReadColorArray(this BinaryReader reader)
+        {
+            var isnull = reader.ReadBoolean();
+            if (isnull)
+                return null;
+            var l = reader.ReadInt32();
+            var array = new Color[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = reader.ReadColor();
+            }
+            return array;
+        }
+
+        public static Mesh ReadMesh(this BinaryReader reader)
+        { 
+            var mesh = new Mesh();
+            mesh.vertices = reader.ReadVector3Array();
+            mesh.triangles = reader.ReadIntArray();
+            mesh.uv = reader.ReadVector2Array();
+            mesh.colors = reader.ReadColorArray();
+            return mesh;
+        }
+
+        #region 保留方案
+        public static T Read<T>(this BinaryReader reader)
+            where T : struct
+        {
+            var size = Marshal.SizeOf<T>();
+            var buffer = reader.ReadBytes(size);
+            if (SerializeHelp.BytesToStruct<T>(buffer, out var result))
+                return result;
+            else
+                throw new InvalidOperationException();
+
+        }
+
+        public static T[] ReadArray<T>(this BinaryReader reader)
+            where T : struct
+        {
+            var isnull = reader.ReadBoolean();
+            if (isnull)
+                return null;
+            var l = reader.ReadInt32();
+            var size = Marshal.SizeOf<T>();
+            var buffer = reader.ReadBytes(size*l);
+            return SerializeHelp.BytesToStructArray<T>(buffer);
+        }
+        #endregion
+    }
+}
+
---
+++ Runtime/Core/Help/BinaryWriter_Extend.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace UnityLib
+{
+    public static class BinaryWriter_Extend
+    {
+        public static void Write(this BinaryWriter writer, Vector2 value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+        }
+        public static void Write(this BinaryWriter writer, Vector3 value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+        }
+        public static void Write(this BinaryWriter writer, Vector4 value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+            writer.Write(value.w);
+        }
+        public static void Write(this BinaryWriter writer, Quaternion value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+            writer.Write(value.w);
+        }
+        public static void Write(this BinaryWriter writer, Vector2Int value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+        }
+        public static void Write(this BinaryWriter writer, Vector3Int value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+        }
+
+        public static void Write(this BinaryWriter writer, Color value)
+        {
+            writer.Write(value.r);
+            writer.Write(value.g);
+            writer.Write(value.b);
+            writer.Write(value.a);
+        }
+
+        public static void Write(this BinaryWriter writer,Color32 value)
+        {
+            writer.Write(value.r);
+            writer.Write(value.g);
+            writer.Write(value.b);
+            writer.Write(value.a);
+        }
+
+        public static void Write(this BinaryWriter writer, Transform value)
+        {
+            if (value == null)
+                throw new ArgumentException("value");
+            writer.Write(value.position);
+            writer.Write(value.rotation);
+            writer.Write(value.localScale);
+        }
+
+        public static void Write(this BinaryWriter writer, AnimationCurve value)
+        {
+            if (value == null)
+            {
+                writer.Write(false);
+            }
+            else
+            {
+                writer.Write(true);
+            }
+
+            writer.Write((int)value.preWrapMode);
+            writer.Write((int)value.postWrapMode);
+            writer.Write(value.length);
+
+            for (int i = 0; i < value.length; i++)
+            {
+                Keyframe keyframe = value.keys[i];
+                writer.Write(keyframe);
+            }
+        }
+
+        public static void Write(this BinaryWriter writer, Keyframe value)
+        {
+            writer.Write(value.time);
+            writer.Write(value.value);
+            writer.Write(value.inTangent);
+            writer.Write(value.outTangent);
+            writer.Write(value.inWeight);
+            writer.Write(value.outWeight);
+            writer.Write((int)value.weightedMode);
+        }
+
+        public static void Write(this BinaryWriter writer, MinMaxCurve value)
+        {
+            writer.Write(value.constant);
+            writer.Write(value.constantMax);
+            writer.Write(value.constantMin);
+            writer.Write(value.curve);
+            writer.Write(value.curveMax);
+            writer.Write(value.curveMin);
+            writer.Write(value.curveMultiplier);
+            //writer.Write(value.curveScalar);
+        }
+
+        //TODO 序列化粒子系统
+        //public static void Write(this BinaryWriter writer, ParticleSystem value) 
+        //{
+        //    if (value == null)
+        //        throw new ArgumentException("value");
+        //    writer.Write(value.main);
+        //}
+
+        //public static void Write(this BinaryWriter writer, ParticleSystem.MainModule module)
+        //{
+        //    writer.Write(module.duration);
+        //    writer.Write(module.loop);
+        //    writer.Write(module.prewarm);
+        //}
+
+        //private static List<Vector3> _v3cache;
+        //public static List<Vector3> v3cache
+        //{
+        //    get
+        //    {
+        //        if (_v3cache == null)
+        //            _v3cache = new List<Vector3>();
+        //        return _v3cache;
+        //    }
+        //}
+        public static void Write(this BinaryWriter writer, Mesh mesh)
+        {
+            if (mesh == null)
+            {
+                writer.Write(false);
+                return;
+            }
+            else
+            {
+                writer.Write(true);
+            }
+            Write(writer, mesh.vertices);
+            Write(writer, mesh.triangles);
+            Write(writer, mesh.uv);
+            Write(writer, mesh.colors);
+        }
+
+        public static void Write(this BinaryWriter writer, Vector3[] array)
+        {
+            if (array == null)
+            { 
+                writer.Write(true);
+                return;
+            }
+            writer.Write(false);
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write(array[i]);
+            }
+        }
+
+        public static void Write(this BinaryWriter writer, Vector2[] array)
+        {
+            if (array == null)
+            {
+                writer.Write(true);
+                return;
+            }
+            writer.Write(false);
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write(array[i]);
+            }
+        }
+
+        public static void Write(this BinaryWriter writer, int[] array)
+        {
+            if (array == null)
+            {
+                writer.Write(true);
+                return;
+            }
+            writer.Write(false);
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write(array[i]);
+            }
+        }
+
+        public static void Write(this BinaryWriter writer, Color[] array)
+        {
+            if (array == null)
+            {
+                writer.Write(true);
+                return;
+            }
+            writer.Write(false);
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write(array[i]);
+            }
+        }
+
+        #region 保留方案
+        public static void Write<T>(this BinaryWriter writer, T value)
+            where T : struct
+        {
+            var bytes = SerializeHelp.StructToBytes(value);
+            writer.Write(bytes);
+        }
+
+        public static void Write<T>(this BinaryWriter writer, T[] array)
+            where T : struct
+        {
+            if (array == null)
+            {
+                writer.Write(true);
+                return;
+            }
+            writer.Write(false);
+            writer.Write(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.Write<T>(array[i]);
+            }
+        }
+        #endregion
+    }
+}
+
